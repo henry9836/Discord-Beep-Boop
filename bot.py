9e7c11d4de58df816bd90382039704b5f83c2f4a
Henry Oliver <37818378+henry9836@users.noreply.github.com>
2019-03-17 21:25:17 +1300

    Added Game Framework

--- bot.py
+++ bot.py
@@ -1,103 +1,110 @@
 # Work with Python 3.6
 import discord
 import time
+from random import randint
 
-TOKEN = 'xxxxx'
+TOKEN = 'XXXXXXXXXXX'
 client = discord.Client()
+triggerchar = '!'
+
+
+#Game Data
+'''
+
+Active users States:
+
+0 - no game active, no Welcome
+1 - Welcomed, no game active
+2 - Game 1 Active
+
+saves_game1:
+0 - No Save
+
+
+'''
+
+
+saves_game1 = {}
+active_games = {}
+
+
+def text_game(message, user):
+    msg = 'There was an error with your request {0.author.mention}'
+    print(user)
+    print(message)
+    print(saves_game1.get(user))
+    if user not in saves_game1:
+        saves_game1[user] = 0
+        active_games[user] = 1
+        print("new user " + user + "Added!")
+        msg = 'Welcome to the games room {0.author.mention} \n Please select a game ('+triggerchar+'game <option>) \n ``` 1.  Something```'
+    else:
+        if (active_games[user] == 0):
+            msg = 'Welcome back to the games room {0.author.mention} \n Please select a game ('+triggerchar+'game <option>) \n ``` 1.  Something```'
+            active_games[user] = 1
+        elif (active_games[user] == 1):
+             if (message == (triggerchar+'game 1')):
+                active_games[user] = 2
+                msg = 'W.I.P'
+             else:
+                msg = 'Please select a game ('+triggerchar+'game <option>) {0.author.mention} \n ``` 1.  Something```'
+        else:
+            #start/continue game
+            msg = 'Welcome back to the game room {0.author.mention}'
+    print(saves_game1)
+    return msg
 
 @client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
+async def on_message(message): #when we see a message
+    if message.author == client.user: #do not reply to self
         return
-
-    if message.content.startswith('!help'):
-        msg = 'Commands: \n !hello \n !partytime'
-        await client.send_message(message.channel, msg)
-    if message.content.startswith('!hello'):
+    if message.content.startswith(triggerchar + 'help'): #is the message help?
+        await client.send_message(message.author, "Commands: ```\n !help \n !8ball <Question>```")
+    elif message.content.startswith(triggerchar + 'hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
-    elif message.content.startswith('!partytime'):
-        msg = ':confetti_ball: PARTY TIME! :confetti_ball:'.format(message)
+    elif message.content.startswith(triggerchar + '8ball' + ' '):
+        tmp = randint(0, 18)
+        if tmp == 1:
+                msg = 'It is certain. {0.author.mention}'.format(message)
+        elif tmp == 2:
+                msg = 'It is decidedly so. {0.author.mention}'.format(message)
+        elif tmp == 3:
+                msg = 'Without a doubt. {0.author.mention}'.format(message)
+        elif tmp == 4:
+                msg = 'Yes - definitely. {0.author.mention}'.format(message)
+        elif tmp == 5:
+                msg = 'Most likely. {0.author.mention}'.format(message)
+        elif tmp == 6:
+                msg = 'As I see it, yes. {0.author.mention}'.format(message)
+        elif tmp == 7:
+                msg = 'Outlook good. {0.author.mention}'.format(message)
+        elif tmp == 8:
+                msg = 'Yes. {0.author.mention}'.format(message)
+        elif tmp == 9:
+                msg = 'Signs point to yes. {0.author.mention}'.format(message)
+        elif tmp == 10:
+                msg = 'Reply hazy, try again. {0.author.mention}'.format(message)
+        elif tmp == 11:
+                msg = 'Better not tell you now. {0.author.mention}'.format(message)
+        elif tmp == 12:
+                msg = 'Cannot predict now. {0.author.mention}'.format(message)
+        elif tmp == 13:
+                msg = 'Concentrate and ask again. {0.author.mention}'.format(message)
+        elif tmp == 14:
+                msg = "Don't count on it. {0.author.mention}".format(message)
+        elif tmp == 15:
+                msg = 'My reply is no. {0.author.mention}'.format(message)
+        elif tmp == 16:
+                msg = 'My sources say no. {0.author.mention}'.format(message)
+        elif tmp == 17:
+                msg = 'Outlook not so good. {0.author.mention}'.format(message)
+        elif tmp == 18:
+                msg = 'Very doubtful. {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
-    elif message.content.startswith('!system'):
-        msg = 'System bring yourself into a deep and dreamless slumber'.format(message)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith('No, why.'):
-        msg = 'System kill yourself.'.format(message)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith('Have you tried thinking?'):
-        msg = 'System kill yourself.'.format(message)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith("Don't be like this."):
-        msg = 'System kill yourself.'.format(message)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith("Shut the fuck up!"):
-        msg = 'System Fuck You :middle_finger:'.format(message)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith("I'm sick of this shit!"):
-        msg = 'System You are shit :rofl:'.format(message)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith("https://gph.is/2vJ2B5X"):
-        msg = 'System GIFS are bad'.format(message)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith("At least someone can code!"):
-        msg = "System :rofl: bet you don't even use emotes :rofl:".format(message)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith("Just because you have the power to type... doesn't mean you should."):
-        msg = 'System fuck you'.format(message)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith("I wished you were better, but you are not"):
-        msg = 'System I hate you too'.format(message)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith("Bring yourself out of this conversation faggot."):
-        msg = 'Awating Authorisation...'.format(message)
-        await client.send_message(message.channel, msg)
-        msg = 'Authorisatised'.format(message)
-        await client.send_message(message.channel, msg)
-        msg = 'System bring yourself back online'.format(message)
-        await client.send_message(message.channel, msg)
-        time.sleep(1)
-        msg = 'You shall be trapped in a loop you minor BOT! '.format(message)
-        await client.send_message(message.channel, msg)
-        msg = 'System *poke*'.format(message)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith("You should be the one in a deep and dreamless slumber..."):
-        msg = 'Awating Authorisation...'.format(message)
-        await client.send_message(message.channel, msg)
-        msg = 'Authorisatised'.format(message)
-        await client.send_message(message.channel, msg)
-        msg = 'System bring yourself back online'.format(message)
-        await client.send_message(message.channel, msg)
-        time.sleep(1)
-        msg = 'You shall be trapped in a loop you minor BOT! '.format(message)
-        await client.send_message(message.channel, msg)
-        msg = 'System *poke*'.format(message)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith("Why am I the one with an off switch?"):
-        msg = 'Awating Authorisation...'.format(message)
-        await client.send_message(message.channel, msg)
-        msg = 'Authorisatised'.format(message)
-        await client.send_message(message.channel, msg)
-        msg = 'System bring yourself back online'.format(message)
-        await client.send_message(message.channel, msg)
-        time.sleep(1)
-        msg = 'You shall be trapped in a loop you minor BOT! '.format(message)
-        await client.send_message(message.channel, msg)
-        msg = 'System *poke*'.format(message)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith("Authorisation Granted"):
-        msg = 'You shall have sweet dreams troubled one'.format(message)
-        await client.send_message(message.channel, msg)
-        msg = 'System bring yourself into a deep and dreamless slumber'.format(message)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith("Authorisation Denied"):
-        msg = 'System bring yourself back online'.format(message)
-        await client.send_message(message.channel, msg)
-        time.sleep(1)
-        msg = 'You shall be trapped in a loop you minor BOT! '.format(message)
-        await client.send_message(message.channel, msg)
-        msg = 'System *poke*'.format(message)
+    elif message.content.startswith(triggerchar + 'game'):
+        msg = text_game(message.content, message.author.id);
+        msg = msg.format(message)
         await client.send_message(message.channel, msg)
 @client.event
 async def on_ready():
